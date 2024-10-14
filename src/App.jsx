b5bbf2aa0389b386507ecd49d3dcd23ba7288263
shiv291023 <shivkumarar4@gmail.com>
2024-10-14 11:31:55 -0400

    my-fourth-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import React, { useState } from 'react';  // Import useState from React
 
+import { BrowserRouter } from "react-router-dom";
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components/components";
+
+
 const App = () => {
   const [count, setCount] = useState(0); // State initialization
 
   return (
-    <div>
-      3D Developer Portfolio
-    </div>
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 };
 
